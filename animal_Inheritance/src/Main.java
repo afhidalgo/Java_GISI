fc6b4f250b2bf020c183cd88e8373357c686b898
afhidalgo <af.hidalgo.romero@gmail.com>
2024-11-20 11:54:15 +0100

    cat class added

--- animal_Inheritance/src/Main.java
+++ animal_Inheritance/src/Main.java
@@ -21,14 +21,39 @@ class Dog extends Animal {
     @Override
     void sound() {
         // Call parent class method
-        super.sound();
+       // super.sound();
         System.out.println(name + " barks.");
+    }
+}
+
+class Cat extends Animal {
+	String age;
+    Cat(String name , String age) {
+        // Call parent class constructor
+        super(name);
+	this.age = age;
+    }
+
+    @Override
+    void sound() {
+        // Call parent class method
+        // super.sound();
+        System.out.println(name + " Meow.");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
         Dog dog = new Dog("Buddy", "3");
-        dog.sound();
+        Cat cat = new Cat("Sunny", "3");
+        //dog.sound();
+        //cat.sound();
+	makeAnimalSound(dog);
+	makeAnimalSound(cat);
     }
+    //Function to make any Animal object to make a sound
+    public static void makeAnimalSound(Animal animal){
+	    animal.sound():
+    }
+
 }
